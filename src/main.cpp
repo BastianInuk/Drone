3f5a889fb4c79f4fd49dc8d430a41fd04b9ce691
Bastian Inuk Christensen <bastian@inuk.blog>
2020-04-26 14:19:28 +0200

    added comments

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,19 @@
 #include "AsyncUDP.h"
 #include "potentiometer.h"
 
-const char * ssid = "OneplusOne"; // put your own SSID here
-const char * password = "inhumanddragon"; // Put your own Wifi Password here
+// Networking values here
+const char * ssid = "GimmeFi"; // put your own SSID here
+const char * password = "1029384756"; // Put your own Wifi Password here
 const IPAddress yourip(192,168,43,134); // Put your own drone/pixel emulator's IP here
 
 int port = 7000;
 int outPort = 4000;
 
-int interval = 1000;
+// Button for changing color
 int btn = 32;
 bool colourChanged = false;
 
-
-
-
-
-int prevTime = 0;
-int currentTime = millis();
-
+// Potentiometers
 Potentiometer xAxis (33); 
 Potentiometer yAxis (34); 
 Potentiometer zAxis (39); // Set to an analogue input
@@ -33,13 +28,17 @@ void setup()
     pinMode(btn, INPUT_PULLUP);
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
+    
+    // Chekc for if WiFi failed and hault the program if WiFi did fail
     if (WiFi.waitForConnectResult() != WL_CONNECTED) 
     {
         Serial.println("WiFi Failed");
+        // This is an infinite loop, which prevents the loop program from working
         while(1) {
             delay(1000);
         }
     }else{
+        // If WiFi succeeded, tell the user so they don't just wait around
         Serial.println("WiFi Connected");
     }
 }
@@ -47,17 +46,19 @@ void setup()
 
 void loop() 
 {
-
+    // Protection, sends only 1 command no matter how long the user presses
     if(digitalRead(btn) == LOW && !colourChanged){
         colourChanged = true;
         Serial.println("change colour");
         udp.writeTo((const uint8_t*)"chClr", 5, yourip, outPort);
+        // Flip the switch, otherwise the button will only ever work once
     } else if (digitalRead(btn) == HIGH && colourChanged) {
         colourChanged = false;
     }
-    delay(0);
+    
+    // Potentiometers sends command here, there's a deadzone of 10% of each potentiometer, so the drone can stand still
     xAxis.doStuff([](int val){
-        if(val > 10) {
+        if(val > 10) { // check for deadzone
             Serial.println("Moving right");
            udp.writeTo((const uint8_t*)"moveR", 5, yourip, outPort);
         } else if (val < -10) {
@@ -87,5 +88,4 @@ void loop()
             udp.writeTo((const uint8_t*)"moveB", 5, yourip, outPort);
         } 
     });
-    udp.writeTo((const uint8_t*)"test", 4, yourip, outPort);
 }
